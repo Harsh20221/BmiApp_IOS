e07d4bd716b129687e1178743f8e56d2906c73d2
Harsh <harsh20221@gmail.com>
2024-07-21 05:05:34 +0530

    Initial Commit

---
+++ BmiApp_IOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BmiApp_IOS
+//
+//  Created by Harsh Kumar on 21/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
