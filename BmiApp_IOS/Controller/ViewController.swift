95c2f1787b544ddb6db0c66df043d4d99e280b2c
Harsh <harsh20221@gmail.com>
2024-07-22 03:43:19 +0530

    Made required files and added logic for view 2

--- BmiApp_IOS/Controller/ViewController.swift
+++ BmiApp_IOS/Controller/ViewController.swift
@@ -1,20 +1,44 @@
 //
 //  ViewController.swift
 //  BMI Calculator
-//
-//  Created by Angela Yu on 21/08/2019.
-//  Copyright © 2019 Angela Yu. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var HeightLabel: UILabel!
+    @IBOutlet var WeightSlider: UISlider!
+    @IBOutlet var WeightLabel: UILabel!
+    @IBOutlet var Heightslider: UISlider!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+///This Action is connected to both slders 
+   
+    @IBAction func HeightSlider(_ sender: UISlider) {
+        HeightLabel.text=String(format:"%.0f",sender.value)
+    }
+    
+    @IBAction func WeightSlider(_ sender: UISlider) {
+        WeightLabel.text=String(format:"%.0f",sender.value)
+    
+    }
+    
+    @IBAction func CalculateButton(_ sender: UIButton) {
+        ////We are Calculating The Bmi Here
+            let height = Heightslider.value
+            let weight=WeightSlider.value
+            let BMI=weight/pow(2, height)
+        
+        let secondVc=SecondViewController()
+        secondVc.bmivalue=String(format:"%.1f",BMI)
+        self.present(secondVc,animated: true,completion: nil)
+        
+    }
+    
+    
+    
+    
 }
 
