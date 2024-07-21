1cca8b7a39aabf365dd4f07f32e1ec415aacd17f
Harsh <harsh20221@gmail.com>
2024-07-22 05:21:15 +0530

    Added Result   Screen that displays Correct BMI

--- BmiApp_IOS/Controller/ResultViewController.swift
+++ BmiApp_IOS/Controller/ResultViewController.swift
@@ -4,10 +4,34 @@
 //
 //  Created by Harsh Kumar on 22/07/24.
 //
+
 import UIKit
-import Foundation
-class ResultViewController:UIViewController {
-    override func viewDidLoad() {
-    super.viewDidLoad()
+
+class ResultViewController: UIViewController {
+    var bmivalue : String?
+    override func viewDidLoad() {  // Add all operational code here
+        super.viewDidLoad()
+        BMIResult.text=bmivalue
+        // Do any additional setup after loading the view.
     }
+    
+    
+    @IBAction func RecalculateButton(_ sender: UIButton) {
+    }
+    @IBOutlet var BMIResult: UILabel!
+ 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view 
+controller.*/
+   
+
 }
+
+
+
+
--- BmiApp_IOS/Controller/ViewController.swift
+++ BmiApp_IOS/Controller/ViewController.swift
@@ -5,7 +5,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet var HeightLabel: UILabel!
     @IBOutlet var WeightSlider: UISlider!
     @IBOutlet var WeightLabel: UILabel!
@@ -14,31 +14,31 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-///This Action is connected to both slders 
-   
+    ///This Action is connected to both slders
+    
     @IBAction func HeightSlider(_ sender: UISlider) {
         HeightLabel.text=String(format:"%.0f",sender.value)
     }
     
     @IBAction func WeightSlider(_ sender: UISlider) {
         WeightLabel.text=String(format:"%.0f",sender.value)
-    
+        
     }
     
     @IBAction func CalculateButton(_ sender: UIButton) {
         ////*We are Calculating The Bmi Here
-            let height = Heightslider.value
-            let weight=WeightSlider.value
-            let BMI=weight/pow(2, height)
-        ///* Here We are Creating a second view controller here using the SecondViewController swift file
-        let secondVc=SecondViewController()
-        secondVc.bmivalue=String(format:"%.1f",BMI) //? Here we are passing the BMI value to the second screen's BMI label
-        self.present(secondVc,animated:  true,completion: nil) //? This Line of code here is responsible for changing the view from Primary screen to secondary screen
+        let height = Heightslider.value
+        let weight=WeightSlider.value
+        let BMI=weight/pow(2, height)
+        self.performSegue(withIdentifier: "goToScreen", sender: self ) //This is segueway initialisation to go to next screen
         
+        
+        func prepare(for segue: UIStoryboardSegue,sender:Any?){
+            if segue.identifier == "goToResult"{
+                let destinationVC = segue
+                    .destination as! ResultViewController
+                destinationVC.bmivalue=String(BMI)
+            }
+        }
     }
-    
-    
-    
-    
 }
-
