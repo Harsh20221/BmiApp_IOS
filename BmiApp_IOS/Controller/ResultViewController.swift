cfeacb83533e96680a5a014012fc2f9c719db3c3
Harsh <harsh20221@gmail.com>
2024-07-22 04:11:55 +0530

    Added Real Result View  Controller

---
+++ BmiApp_IOS/Controller/ResultViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ResultViewController.swift
+//  BmiApp_IOS
+//
+//  Created by Harsh Kumar on 22/07/24.
+//
+import UIKit
+import Foundation
+class ResultViewController:UIViewController {
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    }
+}
