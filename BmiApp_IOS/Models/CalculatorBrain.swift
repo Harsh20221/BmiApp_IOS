ff34e426c53d3b10c2d81e907c8b60b801fd5198
Harsh <harsh20221@gmail.com>
2024-07-22 17:19:24 +0530

    Comments

--- BmiApp_IOS/Models/CalculatorBrain.swift
+++ BmiApp_IOS/Models/CalculatorBrain.swift
@@ -9,12 +9,12 @@ import Foundation
 import UIKit
 var BMI : Float=0.0
 struct CalculatorBrain {
-    
+    ///???This will calculate the BMI
     func calculateBMI(height:Float , weight:Float) -> Float{
          BMI=weight/(height*height)
         return BMI
     }
-    
+    ////? This will format the calculated BMI in Correct form 
     func getBMIValue () -> String{
         let CorrrectBMIFORM=String(format:"%.1f",BMI)
         return CorrrectBMIFORM
